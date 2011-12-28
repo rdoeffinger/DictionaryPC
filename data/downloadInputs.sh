18ebad605e64286706fabb0b842b086a29c0e6ed
Thad Hughes <thad.hughes@gmail.com>
2011-12-27 21:04:12 -0800

    Download changes.

--- data/downloadInputs.sh
+++ data/downloadInputs.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
 
+OLD_DIR=`pwd`
+DIR=`dirname $0`
+
+cd $DIR
+
 echo "Downloading from: http://dumps.wikimedia.org/enwiktionary/"
 WIKI=enwiktionary-20111224-pages-articles.xml
 curl --remote-name http://dumps.wikimedia.org/enwiktionary/20111224/${WIKI}.bz2
+echo "Unzipping... this is slow."
 bunzip2 ${WIKI}.bz2
 mv ${WIKI} inputs/
 
@@ -13,3 +19,5 @@ gunzip ${CHEMNITZ}.gz
 mv ${CHEMNITZ} inputs/
 
 echo "Done.  Now run WiktionarySplitter to spit apart enwiktionary."
+
+cd $OLD_DIR
