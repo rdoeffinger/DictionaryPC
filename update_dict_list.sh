adb896b77554ed67164d3a08caaff859437ac6e0
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2015-08-26 22:20:48 +0200

    Add script to update dictionary list in app.

---
+++ update_dict_list.sh
@@ -0,0 +1,4 @@
+# Run to update ..//Dictionary/res/raw/dictionary_info.txt to reference
+# all dictionaries in /data/outputs (needs to contain both zip and uncompressed files).
+CLASS=CheckDictionariesMain
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -classpath src:../Util/src/:../Dictionary/src/:/usr/share/java/com.ibm.icu.jar:/usr/share/java/xercesImpl.jar com.hughes.android.dictionary.engine.$CLASS "$@"
