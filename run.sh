3cc04aa8f9a343b01d8aa51cb1ae9d4b02b1dee0
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2015-08-28 12:22:43 +0200

    Need at least 4GB heap to create dictionaries.

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -classpath src:../Util/src/:../Dictionary/src/:/usr/share/java/com.ibm.icu.jar:/usr/share/java/xercesImpl.jar:/usr/share/java/commons-lang3.jar com.hughes.android.dictionary.engine.DictionaryBuilder "$@"
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Xmx4096m -classpath src:../Util/src/:../Dictionary/src/:/usr/share/java/com.ibm.icu.jar:/usr/share/java/xercesImpl.jar:/usr/share/java/commons-lang3.jar com.hughes.android.dictionary.engine.DictionaryBuilder "$@"
