923b0e34d1a08c7849f475b691a4b93319ac447a
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2015-08-28 13:03:14 +0200

    Generate more single-language dictionaries.

--- generate_dictionaries.sh
+++ generate_dictionaries.sh
@@ -1,4 +1,5 @@
 #./run.sh --lang1=EN --dictOut=test --dictInfo=test --input0=data/inputs/wikiSplit/en/EN.data  --input0Name=enwikitionary --input0Format=enwiktionary --input0LangPattern=English --input0LangCodePattern=en --input0EnIndex=1 --input0WiktionaryType=EnEnglish
+if true; then
 # Note: using input1 seems to hang for ZH currently!
 while read langcode langname ; do
 lang=$(echo $langcode | tr '[a-z]' '[A-Z]')
@@ -13,5 +14,11 @@ test -e data/inputs/stoplists/${langcode}.txt && stoplist="--lang2Stoplist=data/
 rm data/outputs/EN-${lang}.quickdic.v006.zip
 7z a -mx=9 data/outputs/EN-${lang}.quickdic.v006.zip ./data/outputs/EN-${lang}.quickdic
 done < EN-foreign-dictlist.txt
-./run.sh --lang1=EN --lang1Stoplist=data/inputs/stoplists/en.txt --dictOut=data/outputs/EN.quickdic --dictInfo="Wiktionary-based EN dictionary." --input0=data/inputs/wikiSplit/en/EN.data  --input0Name=enwikitionary --input0Format=enwiktionary --input0LangPattern=English --input0LangCodePattern=en --input0EnIndex=1 --input0WiktionaryType=EnEnglish
+fi
+./run.sh --lang1=EN --lang1Stoplist=data/inputs/stoplists/en.txt --dictOut=data/outputs/EN.quickdic --dictInfo="Wiktionary-based EN dictionary." --input0=data/inputs/wikiSplit/en/EN.data  --input0Name=enwikitionary --input0Format=enwiktionary --input0LangPattern=English --input0LangCodePattern=en --input0EnIndex=1 --input0WiktionaryType=EnEnglish --input2=data/inputs/wikiSplit/en/EN.data --input2Format=WholeSectionToHtmlParser --input2Name=enwikitionary --input2WiktionaryLang=EN --input2TitleIndex=1 --input2WebUrlTemplate=http://en.wiktionary.org/wiki/%s
 7z a -mx=9 data/outputs/EN.quickdic.v006.zip ./data/outputs/EN.quickdic
+for langcode in de fr it es ; do
+lang=$(echo $langcode | tr '[a-z]' '[A-Z]')
+./run.sh --lang1=$lang --lang1Stoplist=data/inputs/stoplists/${langcode}.txt --dictOut=data/outputs/${lang}.quickdic --dictInfo="Wiktionary-based ${lang} dictionary." --input1=data/inputs/wikiSplit/${langcode}/${lang}.data --input1Format=WholeSectionToHtmlParser --input1Name=${langcode}wikitionary --input1WiktionaryLang=$lang --input1TitleIndex=1 --input1WebUrlTemplate=http://${langcode}.wiktionary.org/wiki/%s
+7z a -mx=9 data/outputs/${lang}.quickdic.v006.zip ./data/outputs/${lang}.quickdic
+done
