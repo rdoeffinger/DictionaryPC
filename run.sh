cb2e6c1484ee8545a500339f185fd38e1e420651
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2015-12-13 14:45:13 +0100

    Switch to newer icu4j to fix hang bugs with EN-ZH.

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Xmx4096m -classpath src:../Util/src/:../Dictionary/src/:/usr/share/java/com.ibm.icu.jar:/usr/share/java/xercesImpl.jar:/usr/share/java/commons-lang3.jar com.hughes.android.dictionary.engine.DictionaryBuilder "$@"
+# -agentlib:hprof=heap=sites,depth=20
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Xmx4096m -classpath src:../Util/src/:../Dictionary/src/:/usr/share/java/icu4j-49.1.jar:/usr/share/java/xercesImpl.jar:/usr/share/java/commons-lang3.jar com.hughes.android.dictionary.engine.DictionaryBuilder "$@"
