833da788b419bdcbd3ed0eaea2db99ab2ae55c4f
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2016-01-06 17:15:30 +0100

    More generic run scripts.

--- WiktionarySplitter.sh
+++ WiktionarySplitter.sh
@@ -1,3 +1,9 @@
 # Run after downloading (data/downloadInputs.sh) to generate
 # per-language data files from enwiktionary.
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -classpath src:../Util/src/:../Dictionary/src/:/usr/share/java/com.ibm.icu.jar:/usr/share/java/xercesImpl.jar com.hughes.android.dictionary.engine.WiktionarySplitter "$@"
+ICU4J=/usr/share/java/icu4j-49.1.jar
+test -r "$ICU4J" || ICU4J=/usr/share/icu4j-55/lib/icu4j.jar
+XERCES=/usr/share/java/xercesImpl.jar
+test -r "$XERCES" || XERCES=/usr/share/xerces-2/lib/xercesImpl.jar
+JAVA=/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+test -x "$JAVA" || JAVA=java
+"$JAVA" -classpath src:../Util/src/:../Dictionary/src/:"$ICU4J":"$XERCES" com.hughes.android.dictionary.engine.WiktionarySplitter "$@"
--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 # -agentlib:hprof=heap=sites,depth=20
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Xmx4096m -classpath src:../Util/src/:../Dictionary/src/:/usr/share/java/icu4j-49.1.jar:/usr/share/java/xercesImpl.jar:/usr/share/java/commons-lang3.jar com.hughes.android.dictionary.engine.DictionaryBuilder "$@"
+ICU4J=/usr/share/java/icu4j-49.1.jar
+test -r "$ICU4J" || ICU4J=/usr/share/icu4j-55/lib/icu4j.jar
+XERCES=/usr/share/java/xercesImpl.jar
+test -r "$XERCES" || XERCES=/usr/share/xerces-2/lib/xercesImpl.jar
+COMMONS=/usr/share/java/commons-lang3.jar
+test -r "$COMMONS" || COMMONS=/usr/share/commons-lang-3.3/lib/commons-lang.jar
+JAVA=/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+test -x "$JAVA" || JAVA=java
+"$JAVA" -Xmx4096m -classpath src:../Util/src/:../Dictionary/src/:"$ICU4J":"$XERCES":"$COMMONS" com.hughes.android.dictionary.engine.DictionaryBuilder "$@"
