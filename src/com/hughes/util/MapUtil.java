945daa063c34b972b075860414a395ddfb560576
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2020-04-12 00:50:09 +0200

    Remove unused functions that cause warnings.

--- src/com/hughes/util/MapUtil.java
+++ src/com/hughes/util/MapUtil.java
@@ -16,40 +16,11 @@ package com.hughes.util;
 
 import java.util.Map;
 
-@SuppressWarnings({"WeakerAccess", "unused"})
 public class MapUtil {
-
-    public static <K,V> V safeGet(final Map<K,V> map, K key, V defaultValue) {
-        if (!map.containsKey(key)) {
-            return defaultValue;
-        }
-        return map.get(key);
-    }
-
-    public static <K,V> V safeGetOrPut(final Map<K,V> map, K key, V defaultValue) {
-        if (!map.containsKey(key)) {
-            map.put(key, defaultValue);
-        }
-        return map.get(key);
-    }
-
-    public static <K,V> V safeGet(final Map<K,V> map, K key, Class<V> valueClass) {
-        if (!map.containsKey(key)) {
-            try {
-                map.put(key, valueClass.newInstance());
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return map.get(key);
-    }
-
     public static <K,V> V safeRemove(final Map<K,V> map, K key, V defaultValue) {
         if (!map.containsKey(key)) {
             return defaultValue;
         }
         return map.remove(key);
     }
-
-
 }
