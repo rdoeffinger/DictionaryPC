4659e1f8af8a03bcab43eda23e00e55fa0a2cde8
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2025-08-24 14:13:26 +0200

    Update dictionaries URL

--- src/com/hughes/android/dictionary/engine/CheckDictionariesMain.java
+++ src/com/hughes/android/dictionary/engine/CheckDictionariesMain.java
@@ -13,7 +13,7 @@ import com.hughes.android.dictionary.engine.DictionaryInfo.IndexInfo;
 
 public class CheckDictionariesMain {
 
-    static final String BASE_URL = "https://github.com/rdoeffinger/Dictionary/releases/download/v0.3-dictionaries/";
+    static final String BASE_URL = "https://github.com/rdoeffinger/Dictionary/releases/download/v0.4-dictionaries/";
     static final String VERSION_CODE_OLD = "v006";
     static final String VERSION_CODE = "v007";
 
