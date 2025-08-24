ebe867674e39a79f02a928e2e8ee89b46f4225e8
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2025-08-24 12:17:21 +0200

    Update URLs. Wiktionary dumps require HTTPS now.

--- data/downloadInputs.sh
+++ data/downloadInputs.sh
@@ -7,44 +7,44 @@ cd $DIR
 echo "Note that unzipping is slow."
 
 L=en
-echo "Downloading from: http://dumps.wikimedia.org/${L}wiktionary/"
+echo "Downloading from: https://dumps.wikimedia.org/${L}wiktionary/"
 WIKI=${L}wiktionary-latest-pages-articles.xml
-curl -L --remote-name http://dumps.wikimedia.org/${L}wiktionary/latest/${WIKI}.bz2
+curl -L --remote-name https://dumps.wikimedia.org/${L}wiktionary/latest/${WIKI}.bz2
 mv ${WIKI}.bz2 inputs/${L}wiktionary-pages-articles.xml.bz2
 
-echo "Downloading from: http://ftp.tu-chemnitz.de/pub/Local/urz/ding/de-en-devel/"
+echo "Downloading from: https://ftp.tu-chemnitz.de/pub/Local/urz/ding/de-en-devel/"
 CHEMNITZ=de-en.txt
-curl -L --remote-name http://ftp.tu-chemnitz.de/pub/Local/urz/ding/de-en-devel/${CHEMNITZ}.gz
+curl -L --remote-name https://ftp.tu-chemnitz.de/pub/Local/urz/ding/de-en-devel/${CHEMNITZ}.gz
 mv ${CHEMNITZ}.gz inputs/de-en_chemnitz.txt.gz
 
 L=fr
-echo "Downloading from: http://dumps.wikimedia.org/${L}wiktionary/"
+echo "Downloading from: https://dumps.wikimedia.org/${L}wiktionary/"
 WIKI=${L}wiktionary-latest-pages-articles.xml
-curl -L --remote-name http://dumps.wikimedia.org/${L}wiktionary/latest/${WIKI}.bz2
+curl -L --remote-name https://dumps.wikimedia.org/${L}wiktionary/latest/${WIKI}.bz2
 mv ${WIKI}.bz2 inputs/${L}wiktionary-pages-articles.xml.bz2
 
 L=it
-echo "Downloading from: http://dumps.wikimedia.org/${L}wiktionary/"
+echo "Downloading from: https://dumps.wikimedia.org/${L}wiktionary/"
 WIKI=${L}wiktionary-latest-pages-articles.xml
-curl -L --remote-name http://dumps.wikimedia.org/${L}wiktionary/latest/${WIKI}.bz2
+curl -L --remote-name https://dumps.wikimedia.org/${L}wiktionary/latest/${WIKI}.bz2
 mv ${WIKI}.bz2 inputs/${L}wiktionary-pages-articles.xml.bz2
 
 L=de
-echo "Downloading from: http://dumps.wikimedia.org/${L}wiktionary/"
+echo "Downloading from: https://dumps.wikimedia.org/${L}wiktionary/"
 WIKI=${L}wiktionary-latest-pages-articles.xml
-curl -L --remote-name http://dumps.wikimedia.org/${L}wiktionary/latest/${WIKI}.bz2
+curl -L --remote-name https://dumps.wikimedia.org/${L}wiktionary/latest/${WIKI}.bz2
 mv ${WIKI}.bz2 inputs/${L}wiktionary-pages-articles.xml.bz2
 
 L=es
-echo "Downloading from: http://dumps.wikimedia.org/${L}wiktionary/"
+echo "Downloading from: https://dumps.wikimedia.org/${L}wiktionary/"
 WIKI=${L}wiktionary-latest-pages-articles.xml
-curl -L --remote-name http://dumps.wikimedia.org/${L}wiktionary/latest/${WIKI}.bz2
+curl -L --remote-name https://dumps.wikimedia.org/${L}wiktionary/latest/${WIKI}.bz2
 mv ${WIKI}.bz2 inputs/${L}wiktionary-pages-articles.xml.bz2
 
 L=pt
-echo "Downloading from: http://dumps.wikimedia.org/${L}wiktionary/"
+echo "Downloading from: https://dumps.wikimedia.org/${L}wiktionary/"
 WIKI=${L}wiktionary-latest-pages-articles.xml
-curl -L --remote-name http://dumps.wikimedia.org/${L}wiktionary/latest/${WIKI}.bz2
+curl -L --remote-name https://dumps.wikimedia.org/${L}wiktionary/latest/${WIKI}.bz2
 mv ${WIKI}.bz2 inputs/${L}wiktionary-pages-articles.xml.bz2
 
 echo "Done.  Now run WiktionarySplitter to split apart enwiktionary."
